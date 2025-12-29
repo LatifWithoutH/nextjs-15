ec3f1d0fe6a5e6e7f7100564cc36b97d676ccc30
LatifWithoutH <latifsmart123@gmail.com>
2025-12-29 22:01:31 +0700

    Add Metadata for Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 import { Suspense } from "react";
+import { Metadata } from "next";
 
 export const meta Metadata = {
   title: "Login",
